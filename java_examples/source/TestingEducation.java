4ea234c52f9d635188bfcc3ebe0e3da0f8c6f2a8
Vtszet <the@vtszet.com>
2023-08-25 01:28:07 +0300

    Почти доделал. Постараться переделать метод: ArrayList<Boolean> getBinaryList(int iterNumber) так, чтобы состояние буфера в нём кэшировалось, и возвращалась кэшированная часть от него в качестве результата метода.
    Также нужно произвести рефакторинг -- по возможности.

--- java_examples/source/TestingEducation.java
+++ java_examples/source/TestingEducation.java
@@ -3,102 +3,62 @@ import java.util.*;
 
 public class TestingEducation
 {
-    private boolean passed;
+    private static final String ERROR = "Задайте 1 целочисленный параметр больше 0!";
+    private static final String UNKNOWN_ERROR = "Неизвестная ошибка!";
+    private static final String ADDED_SYMBOL = "0";
     
+    private static final int LIMIT_ARGS = 1;
+    private static final int ONLY_ONE_ARG = 0;
+    private static final int FAIL_EXIT_CODE = 1;
+    private static final int DEGREE_BASE = 2;
+
+    private int casesQuantity;
+    private int iterCount;
+           
     
     public static void main(String[] args)
     {
-        showResult(Integer.parseInt(args[0]));
+        if (args.length != LIMIT_ARGS)
+        {     
+            showErrorMessage(ERROR);
+        }
+        
+        try
+        {
+            int casesQuantity = Integer.parseInt(args[ONLY_ONE_ARG]);
+            new TestingEducation(casesQuantity).showResult();
+        }
+        catch (NumberFormatException ex)
+        {
+            showErrorMessage(ERROR);
+        }
+        catch (Exception ex)
+        {
+            showErrorMessage(UNKNOWN_ERROR);
+        }
     }
     
     
-    private static int convertDecToBin(int converted)
+    private static void showErrorMessage(String msg)
     {
-        int result = 0;
+        System.err.println(msg);
+        System.exit(FAIL_EXIT_CODE);
+    }
+    
+    
+    public TestingEducation(int casesQuantity)
+    {   
+        this.casesQuantity = casesQuantity;
+        iterCount = (int) Math.pow(DEGREE_BASE, casesQuantity);   
+    }
+    
+    
+    public void showResult()
+    {
+        System.out.println();
+        int allTestCasesIter = 0;
            
-        for (int count = 0; converted > 0; converted >>= 1, ++count)
-        {
-            result += (converted % 2) * ((int) Math.pow(10, count));
-        }
-       
-        return result;
-    }
-    
-    
-    private static ArrayList<Boolean> convertPsevdoBinToBoolList(int converted)
-    {
-        ArrayList<Boolean> result = new ArrayList<Boolean>();
-        ArrayList<Boolean> buffer = new ArrayList<Boolean>();
-        
-        if (converted == 0)
-        {
-            result.add(convertByteToBool((byte)0));
-            return result;
-        }
-               
-        for (; converted > 0; converted /= 10)
-        {
-            result.add(convertByteToBool((byte)(converted % 10)));
-        }
-        
-        
-        for (int i = result.size() - 1; i >= 0; --i)
-        {
-            buffer.add(result.get(i));
-        }
-        
-        result = buffer;
-        
-        return result;
-    }
-    
-    
-    private static ArrayList<Boolean> getBinaryList(int quantity, int iterNumber,
-                                                    int iterCount)
-    {
-        ArrayList<Boolean> result = new ArrayList<Boolean>();
-        ArrayList<ArrayList<Boolean>> buffer = new ArrayList<ArrayList<Boolean>>();
-        
-        for (int i = 0; i < iterCount; ++i)
-        {
-            for (int j = 0; j < quantity; ++j)
-            {        
-                buffer.add(convertPsevdoBinToBoolList(
-                               convertDecToBin(iterNumber)));
-            }
-        }
-        
-        result = buffer.get(iterNumber); 
-            
-        return result;
-    }
-    
-    
-    private static ArrayList<ArrayList<Boolean>> getAllTestCases(int casesQuantity)
-    {    
-        ArrayList<ArrayList<Boolean>> result = 
-            new ArrayList<ArrayList<Boolean>>();
-        
-        int iterCount = (int) Math.pow(2, casesQuantity);
-        
-        for (int i = 0; i < iterCount; ++i)
-        { 
-            result.add(getBinaryList(casesQuantity, i, iterCount));
-        }
-
-        return result;
-    }
-    
-    
-    private static boolean convertByteToBool(byte converted)
-    {
-        return converted == 0 ? false : true;
-    }
-    
-    
-    private static void showResult(int quantity)
-    { 
-        for (var currentCasesList: getAllTestCases(quantity))
+        for (var currentCasesList: getAllTestCases())
         {
             System.out.println("Current testing cases:");
                       
@@ -108,26 +68,115 @@ public class TestingEducation
                     currentCasesList.get(i));
             }
             
-            System.out.println("Testing results: " + 
-                new TestingEducation(currentCasesList));
-            System.out.println();
+            System.out.println("Testing results: " + getResult(currentCasesList));
+            
+            if (allTestCasesIter < iterCount - 1)
+            {
+                System.out.println();
+            }
+            
+            allTestCasesIter++;
         }
     }
     
     
-    public TestingEducation(ArrayList<Boolean> casesList)
+    private ArrayList<ArrayList<Boolean>> getAllTestCases()
+    {    
+        var result = new ArrayList<ArrayList<Boolean>>();
+        
+        for (int i = 0; i < iterCount; ++i)
+        { 
+            result.add(getBinaryList(i));
+        }
+
+        return result;
+    }
+    
+    
+    private ArrayList<Boolean> getBinaryList(int iterNumber)
     {
-        passed = true;
+        var result = new ArrayList<Boolean>();
+        var buffer = new ArrayList<ArrayList<Boolean>>();
+        String cached = "";
+        
+        for (int i = 0; i < iterCount; ++i)
+        {
+            for (int j = 0; j < casesQuantity; ++j)
+            {   
+                cached = "" + convertDecToBin(iterNumber);        
+                cached = addZeroDigitSymbolIntoBegin(cached);        
+                buffer.add(convertStringToBoolList(cached));   
+            }
+        }
+        
+        result = buffer.get(iterNumber); 
+        return result;
+    }
+    
+    
+    private int convertDecToBin(int converted)
+    {
+        int result = 0;
+           
+        for (int count = 0; converted > 0; converted >>= 1, ++count)
+        {
+            result += (converted % DEGREE_BASE) * ((int) Math.pow(10, count));
+        }
+       
+        return result;
+    }
+    
+    
+    private String addZeroDigitSymbolIntoBegin(String str)
+    {    
+        int strSize = str.length();
+             
+        if (casesQuantity == strSize)
+        {
+            return str;
+        }
+        
+        String result = "";   
+        
+        for (int i = 0; i < casesQuantity - strSize; ++i)
+        {
+            result += ADDED_SYMBOL;
+        }
+        
+        result += str;
+        
+        return result;
+    }
+    
+  
+    private ArrayList<Boolean> convertStringToBoolList(String converted)
+    {
+        var result = new ArrayList<Boolean>();
+    
+        for (char currentSymbol: converted.toCharArray())
+        {
+            result.add(convertCharToBool(currentSymbol));
+        }
+        
+        return result;
+    }
+    
+    
+    private boolean convertCharToBool(char converted)
+    {
+        return converted == '0' ? false : true;
+    }
+    
+       
+    private String getResult(ArrayList<Boolean> casesList)
+    {
+        boolean passed = true;
         
         for (boolean currentCase: casesList)
         {
             passed &= currentCase;
         }
-    }
-    
-    
-    public String toString()
-    {
+        
         return passed ? "OK" : "FAIL";
     }
 }
