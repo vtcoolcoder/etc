1ecebfa603717d7ad00bd59501e4a5ba23903173
Vtszet <the@vtszet.com>
2023-08-27 20:47:34 +0300

    Расширил пример. Стал лучше понимать написание лямбда-выражений.

--- java_examples/source/LambdaNewer.java
+++ java_examples/source/LambdaNewer.java
@@ -1,5 +1,11 @@
 public class LambdaNewer
-{    
+{ 
+    public interface Function
+    {
+        public double run(int n);
+    }
+   
+   
     public static void main(String[] args)
     {
         highOrderMethod(
@@ -7,6 +13,16 @@ public class LambdaNewer
             LambdaNewer::showArithmeticProgression,
             LambdaNewer::showGeometicProgression,
             LambdaNewer::showGoodBye
+        );
+        
+        showCustomProgression(1000, 1101, 1,
+            LambdaNewer::getSelf,
+            LambdaNewer::getSqr,
+            LambdaNewer::getCube,
+            LambdaNewer::getSqrt,
+            LambdaNewer::getCubeRoot,
+            LambdaNewer::getPentaRoot,
+            LambdaNewer::getSeptaRoot
         );
     }
     
@@ -49,5 +65,68 @@ public class LambdaNewer
             System.out.print(i*i + " ");
         }
         System.out.println();
+    }
+    
+    
+    private static void showCustomProgression(int start, int stop, int step, 
+                                              Function ... functions)
+    {
+        for (Function function: functions)
+        {
+            System.out.println();
+              
+            for (int i = start; i < stop; i += step)
+            {
+                System.out.print(function.run(i) + "    ");
+                if (i % 4 == 0)
+                {
+                    System.out.println();
+                }
+            }
+            
+            System.out.println();
+        }
+    }
+    
+    
+    private static double getSelf(int n)
+    {
+        return n;
+    }
+    
+    
+    private static double getSqr(int n)
+    {
+        return n*n;
+    }
+    
+    
+    private static double getCube(int n)
+    {
+        return n*n*n;
+    }
+    
+    
+    private static double getSqrt(int n)
+    {
+        return Math.sqrt(n);
+    }
+    
+    
+    private static double getCubeRoot(int n)
+    {
+        return Math.pow(n, 1.0/3.0);
+    }
+    
+    
+    private static double getPentaRoot(int n)
+    {
+        return Math.pow(n, 1.0/5.0);
+    }
+    
+    
+    private static double getSeptaRoot(int n)
+    {
+        return Math.pow(n, 1.0/7.0);
     }
 }
