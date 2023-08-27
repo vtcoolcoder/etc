253c24cc94a0d2acf64ab5634babd8a4395d0a83
Vtszet <the@vtszet.com>
2023-08-28 00:16:21 +0300

    Произвёл рефакторинг в основном.

--- java_examples/source/LambdaNewer.java
+++ java_examples/source/LambdaNewer.java
@@ -1,46 +1,73 @@
-
-
 public class LambdaNewer
 { 
+    public interface Function
+    {
+        double run(double t);
+    }
+    
     private static final String GREETING = "Hello, world!";
     private static final String BYE = "Good bye!";
 
-    private static final double SQR = 1.0/2.0;
-    private static final double CUBE = 1.0/3.0;
-    private static final double PENTA = 1.0/5.0;
-    private static final double SEPTA = 1.0/7.0;
-    private static final double UNDECIMA = 1.0/11.0;
-    private static final double TERCDECIMA = 1.0/13.0;
-   
+    private static final double SQR_ROOT = getRelation(2);
+    private static final double CUBE_ROOT = getRelation(3);
+    private static final double PENTA_ROOT = getRelation(5);
+    private static final double SEPTA_ROOT = getRelation(7);
+    private static final double UNDECIMA_ROOT = getRelation(11);
+    private static final double TERCDECIMA_ROOT = getRelation(13);
     
-    public interface Function
+    private static final int LENGTH = 20;
+    private static final int ROOT_START = 1_000_000;
+    private static final int ROOT_STEP = 500_000;
+    private static final int ROOT_STOP = getStop(LENGTH, ROOT_START, ROOT_STEP);
+    private static final int DEGREE_START = 0;
+    private static final int DEGREE_STEP = 1;
+    private static final int DEGREE_STOP = LENGTH;        
+     
+     
+    private static final Runnable[] VOID_FUNCTIONS =
     {
-        public double run(double n);
-    }
-   
-   
+        LambdaNewer::showGreeting,
+        LambdaNewer::showArithmeticProgression,
+        LambdaNewer::showGeometicProgression,
+        LambdaNewer::showGoodBye
+    };
+    
+    private static final Function[] DEGREE_FUNCTIONS =
+    {    
+        LambdaNewer::getSelf,
+        LambdaNewer::getSqr,
+        LambdaNewer::getCube
+    };   
+    
+    private static final Function[] ROOT_FUNCTIONS =
+    {
+        LambdaNewer::getSqrt,
+        LambdaNewer::getCubeRoot,
+        LambdaNewer::getPentaRoot,
+        LambdaNewer::getSeptaRoot,
+        LambdaNewer::getUndecimaRoot,
+        LambdaNewer::getTercdecimaRoot
+    };
+    
+      
     public static void main(String[] args)
     {
-        highOrderMethod(
-            LambdaNewer::showGreeting,
-            LambdaNewer::showArithmeticProgression,
-            LambdaNewer::showGeometicProgression,
-            LambdaNewer::showGoodBye
-        );
-        
-        showCustomProgression(0, 21, 1,
-            LambdaNewer::getSelf,
-            LambdaNewer::getSqr,
-            LambdaNewer::getCube);
-            
-        showCustomProgression(1_000_000, 10_000_000, 500_000,    
-            LambdaNewer::getSqrt,
-            LambdaNewer::getCubeRoot,
-            LambdaNewer::getPentaRoot,
-            LambdaNewer::getSeptaRoot,
-            LambdaNewer::getUndecimaRoot,
-            LambdaNewer::getTercdecimaRoot
-        );
+        highOrderMethod(VOID_FUNCTIONS);
+        showCustomProgression(
+            DEGREE_START, DEGREE_STOP, DEGREE_STEP, DEGREE_FUNCTIONS);       
+        showCustomProgression(ROOT_START, ROOT_STOP, ROOT_STEP, ROOT_FUNCTIONS);   
+    }
+    
+    
+    private static double getRelation(double n)
+    {
+        return 1./n;
+    }
+    
+    
+    private static int getStop(int length, int start, int step)
+    {
+        return step*(length + 1) + start;
     }
     
     
@@ -92,10 +119,11 @@ public class LambdaNewer
         {
             System.out.println();
               
-            for (int i = start; i < stop; i += step)
+            for (double i = start; i < stop; i += step)
             {
-                System.out.print(function.run(i) + "    ");
-                if (i % 4 == 0)
+                System.out.print(function.run(i) + "    ");       
+                int offset = ((int)i - start) / step;  
+                if (++offset % 4 == 0)
                 {
                     System.out.println();
                 }
@@ -123,45 +151,39 @@ public class LambdaNewer
         return n*n*n;
     }
     
-    
-    private static double getRoot(double base, double degree)
-    {
-        return Math.pow(base, degree);
-    }
-    
-    
+      
     private static double getSqrt(double n)
     {
-        return getRoot(n, SQR);
+        return Math.pow(n, SQR_ROOT);
     }
     
     
     private static double getCubeRoot(double n)
     {
-        return getRoot(n, CUBE);
+        return Math.pow(n, CUBE_ROOT);
     }
     
     
     private static double getPentaRoot(double n)
     {
-        return getRoot(n, PENTA);
+        return Math.pow(n, PENTA_ROOT);
     }
     
     
     private static double getSeptaRoot(double n)
     {
-        return getRoot(n, SEPTA);
+        return Math.pow(n, SEPTA_ROOT);
     }
     
     
     private static double getUndecimaRoot(double n)
     {
-        return getRoot(n, UNDECIMA);
+        return Math.pow(n, UNDECIMA_ROOT);
     }
     
     
     private static double getTercdecimaRoot(double n)
     {
-        return getRoot(n, TERCDECIMA);
+        return Math.pow(n, TERCDECIMA_ROOT);
     }
 }
