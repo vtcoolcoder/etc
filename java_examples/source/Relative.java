c4b0d2e20dcc03356cc5d2cc42991e53525cd035
Vtszet <the@vtszet.com>
2023-08-28 19:55:32 +0300

    Произвёл очередной рефакторинг.

--- java_examples/source/Relative.java
+++ java_examples/source/Relative.java
@@ -8,45 +8,52 @@ public class Relative
         void run(double n);
     }
     
-      
-    private static final String ERROR = "Нельзя делить на ноль!";
-    private static final String MUSTBE_3_ARGS = "Задайте 3 обязательных аргумента!";
-    private static final String ILLEGAL_NUMBER_FORMAT = "Ошибка формата числа!";
+    private static class RelationParams
+    {
+        private static final int NUMERATOR_BEGIN = 1;
+        private static final int NUMERATOR_END = 9;
+        private static final int DENOMERATOR_BEGIN = NUMERATOR_BEGIN + 1;
+        private static final int DENOMERATOR_END = NUMERATOR_END + 1;
+    } 
     
-    private static final int NUMERATOR_BEGIN = 1;
-    private static final int NUMERATOR_END = 9;
-    private static final int DENUMERATOR_BEGIN = NUMERATOR_BEGIN + 1;
-    private static final int DENUMERATOR_END = NUMERATOR_END + 1;
     private static final int FAIL_EXIT_CODE = 1;
+    private static final int START_ARG_POSITION = 0;
+    private static final int STOP_ARG_POSITION = 1;
+    private static final int STEP_ARG_POSITION = 2;
+    private static final int ARGS_LENGTH_LIMIT = 3;
+      
+    private static final String DIVISION_BY_ZERO = "Нельзя делить на ноль!";
+    private static final String MUSTBE_3_ARGS = 
+        "Задайте 3 обязательных аргумента командной строки: start stop step\n" +
+        "-- для генерации числового ряда с шагом step от числа start до числа " +
+        "stop -- включительно.";
+    private static final String ILLEGAL_NUMBER_FORMAT = "Ошибка формата числа!";
+        
     
-    
-    private static double start;
-    private static double stop;
-    private static double step;
+    private static double start, stop, step;
     private static ArrayList<Double> relations, numbers;
     
         
     public static void main(String[] args)
     {      
-        checkArgs(args);   
+        validateArgs(args);   
         generateNumbers();
-        initRelations(NUMERATOR_BEGIN, NUMERATOR_END, 
-                      DENUMERATOR_BEGIN, DENUMERATOR_END);
+        initRelations(new RelationParams());
         showResults(System.out::println, Relative::printPows);
     }
     
     
-    private static void checkArgs(String[] args)
+    private static void validateArgs(String[] args)
     {
-        if (args.length != 3)
+        if (args.length != ARGS_LENGTH_LIMIT)
         {
             showErrorMessage(MUSTBE_3_ARGS);
         }
         try
         {
-            start = Double.parseDouble(args[0]);
-            stop = Double.parseDouble(args[1]);
-            step = Double.parseDouble(args[2]);
+            start = Double.parseDouble(args[START_ARG_POSITION]);
+            stop = Double.parseDouble(args[STOP_ARG_POSITION]);
+            step = Double.parseDouble(args[STEP_ARG_POSITION]);
         }
         catch (NumberFormatException ex)
         {
@@ -73,14 +80,15 @@ public class Relative
     }
     
     
-    private static void initRelations(double numeratorBegin, double numeratorEnd,
-                                      double denominatorBegin, double denominatorEnd)
+    private static void initRelations(RelationParams params)
     {
         relations = new ArrayList<>();
         
-        for (double num = numeratorBegin; num <= numeratorEnd; ++num)
+        for (double num = params.NUMERATOR_BEGIN; 
+                    num <= params.NUMERATOR_END; ++num)
         {
-            for (double den = denominatorBegin; den <= denominatorEnd; ++den)
+            for (double den = params.DENOMERATOR_BEGIN; 
+                        den <= params.DENOMERATOR_END; ++den)
             {
                 double cachedRelation = relate(num, den);
                 if (isPass(num, den, cachedRelation))
@@ -97,7 +105,7 @@ public class Relative
     {
         if (denominator == 0)
         {
-            throw new IllegalArgumentException(ERROR);
+            throw new IllegalArgumentException(DIVISION_BY_ZERO);
         }
         return numerator / denominator;
     }
