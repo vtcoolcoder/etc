2c24b5791b81207de7d35aa7aa8a4a721a847928
Vtszet <the@vtszet.com>
2023-10-20 14:36:50 +0300

    Добавил больше вариаций переходов по меткам посредством оператора break.

--- java_examples/TestBreak.java
+++ java_examples/TestBreak.java
@@ -10,22 +10,37 @@ public class TestBreak
             MYLABEL:
             {
                 if (args[0].equals("0")) { break MYLABEL; }
-                System.out.println("Inside the label \"MYLABEL\".");
+                System.out.println("\nInside the label \"MYLABEL\".");
+                
                 if (args[0].equals("1")) { break MYLABEL; }
                 for (int i = 0; i < 10; i++) { System.out.print(i); }
-                System.out.println();
+                
+                if (args[0].equals("2")) { break MYLABEL; }
+                System.out.println(
+                    "\nThe end of codeblock inside the label \"MYLABEL\"");
+                    
+                if (args[0].equals("3")) { break BASELABEL; }
             }
             
             MYLABEL2:
             {
                 if (args[1].equals("0")) { break MYLABEL2; }
-                System.out.println("Inside the label \"MYLABEL2\".");
+                System.out.println("\nInside the label \"MYLABEL2\".");
+                
                 if (args[1].equals("1")) { break MYLABEL2; }
                 for (char i = 'A'; i <= 'Z'; i++) { System.out.print(i); }
-                System.out.println();
+                
+                if (args[1].equals("2")) { break MYLABEL2; }
+                System.out.println(
+                    "\nThe end of codeblock inside the label \"MYLABEL2\"");
+                    
+                if (args[1].equals("3")) { break BASELABEL; }
             }
+            
+            System.out.println(
+                "\nThe end of codeblock inside the label \"BASELABEL\"");
         }
         
-        System.out.println("Bye!");
+        System.out.println("Bye!\n");
     }
 }
