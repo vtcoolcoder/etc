ab4d8d2ad95e4fd35b1e308c56645cfa50887ed5
Vtszet <the@vtszet.com>
2023-11-06 17:58:02 +0300

    Доработал предыдущий пример.

--- java_examples/FasterEvalDemo.java
+++ java_examples/FasterEvalDemo.java
@@ -1,28 +1,25 @@
 public class FasterEvalDemo {
+    private static final String GREETING = "Hello, World!";
+    private static final String ASK = "World, how are you?";
+    private static final String BYE = "Bye, World!";
     private static final String TS = "\t\t\t";
     private static final String THE_AND = TS + "AND";
     private static final String THE_OR = TS + "OR";
     private static final String THE_XOR = TS + "XOR";
+    private static final String RESULT = TS + "RESULT: ";       
     
     @FunctionalInterface
-    private interface Boolizable {
-        boolean func(String msg, boolean flag);
+    private interface Messager {
+        boolean message(String msg, boolean flag);
     }
     
     @FunctionalInterface
     private interface Dispatcher {
-        boolean dispatch(Boolizable a);
+        boolean dispatch(Messager messager);
     }
     
-    private enum Messages {
-        GREETING("Hello, World!"),
-        ASK("World, how are you?"),
-        BYE("Bye, World!");
-        
-        private String message;
-        
-        Messages(String msg) { message = msg; }
-        public String getMessage() { return message; }
+    private enum Operators {
+        AND, OR, XOR
     }
 
     private enum MatrixBools {
@@ -61,52 +58,68 @@ public class FasterEvalDemo {
         for (MatrixBools item : MatrixBools.values()) {
             MatrixBools.Params cached = item.getParams();
             System.out.println(cached);
-            showTestedMessages(cached);
+            showMessages(cached);
             System.out.println();
         }   
     }
     
-    private static void showTestedMessages(MatrixBools.Params params) {             
-        String msgA = Messages.GREETING.getMessage();
-        String msgB = Messages.ASK.getMessage();
-        String msgC = Messages.BYE.getMessage();
+    private static void showMessages(MatrixBools.Params params) {
+        Dispatcher theAnd = getDispatcher(params, THE_AND, Operators.AND);
+        Dispatcher theOr = getDispatcher(params, THE_OR, Operators.OR);
+        Dispatcher theXor = getDispatcher(params, THE_XOR, Operators.XOR);
         
-        boolean flagA = params.first;
-        boolean flagB = params.second;
-        boolean flagC = params.third;
-        
-        Dispatcher theAnd = (a) -> {
-            System.out.println(THE_AND);
-            return
-                a.func(msgA, flagA) && 
-                a.func(msgB, flagB) && 
-                a.func(msgC, flagC);
-        };
-                
-        Dispatcher theOr = (a) -> {
-            System.out.println(THE_OR);
-            return 
-                a.func(msgA, flagA) || 
-                a.func(msgB, flagB) || 
-                a.func(msgC, flagC);
-        };
-                
-        Dispatcher theXor = (a) -> {
-            System.out.println(THE_XOR);
-            return
-                a.func(msgA, flagA) ^ 
-                a.func(msgB, flagB) ^ 
-                a.func(msgC, flagC);
-        };
-        
-        getTripleX(theAnd, theOr, theXor);
+        System.out.println(RESULT + getTripleX(theAnd, theOr, theXor));
     }
     
-    private static void getTripleX(Dispatcher ... dsps) {
+    private static Dispatcher 
+    getDispatcher(MatrixBools.Params params, String title, Operators op) {
+        Dispatcher result = null;
+        
+        switch (op) {
+            case AND:
+                result = (messager) -> {
+                    System.out.println(title);
+                    return
+                        messager.message(GREETING, params.first) && 
+                        messager.message(ASK, params.second) && 
+                        messager.message(BYE, params.third);
+                };
+                break;
+                
+            case OR:
+                result = (messager) -> {
+                    System.out.println(title);
+                    return
+                        messager.message(GREETING, params.first) || 
+                        messager.message(ASK, params.second) || 
+                        messager.message(BYE, params.third);
+                };
+                break;
+                
+            case XOR:
+                result = (messager) -> {
+                    System.out.println(title);
+                    return
+                        messager.message(GREETING, params.first) ^ 
+                        messager.message(ASK, params.second) ^ 
+                        messager.message(BYE, params.third);
+                };
+                break;           
+        }
+    
+        return result;
+    }
+    
+    private static boolean getTripleX(Dispatcher ... dsps) {
+        boolean result = true;
+        
         for (Dispatcher dsp : dsps) {
-            System.out.println(
-                TS + dsp.dispatch(FasterEvalDemo::message) + '\n');
-        }                  
+            boolean cached = dsp.dispatch(FasterEvalDemo::message);
+            result &= cached;
+            System.out.println(TS + cached + '\n');
+        }
+        
+        return result;                  
     }
     
     private static boolean message(String msg, boolean flag) {
