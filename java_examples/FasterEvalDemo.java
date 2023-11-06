a1c4362ecccdff1717a4401bbd19ca5433378678
Vtszet <the@vtszet.com>
2023-11-06 19:22:31 +0300

    Произвёл некоторый рефакторинг...

--- java_examples/FasterEvalDemo.java
+++ java_examples/FasterEvalDemo.java
@@ -3,9 +3,6 @@ public class FasterEvalDemo {
     private static final String ASK = "World, how are you?";
     private static final String BYE = "Bye, World!";
     private static final String TS = "\t\t\t";
-    private static final String THE_AND = TS + "AND";
-    private static final String THE_OR = TS + "OR";
-    private static final String THE_XOR = TS + "XOR";
     private static final String RESULT = TS + "RESULT: ";       
     
     @FunctionalInterface
@@ -19,7 +16,10 @@ public class FasterEvalDemo {
     }
     
     private enum Operators {
-        AND, OR, XOR
+        AND("AND"), OR("OR"), XOR("XOR");
+        private String title;
+        Operators(String s) { title = s; }
+        public String getTitle() { return TS + '\t' + title; }
     }
 
     private enum MatrixBools {
@@ -37,21 +37,41 @@ public class FasterEvalDemo {
             private boolean second;
             private boolean third;
             
+            public Params(boolean f, boolean s, boolean t) {
+                first = f;
+                second = s;
+                third = t;
+            }
+            
             public String toString() {
                 return "(" + first + ", " + second + ", " + third + "):";
             }
+            
+            public boolean getFirst() { return first; }
+            public boolean getSecond() { return second; }
+            public boolean getThird() { return third; }
         }
         
         private Params params;
     
-        MatrixBools(boolean f, boolean s, boolean t) {
-            params = new Params();
-            params.first = f;
-            params.second = s;
-            params.third = t;
+        MatrixBools(boolean first, boolean second, boolean third) {
+            params = new Params(first, second, third);    
         }
         
         public Params getParams() { return params; }
+    }
+    
+    private static class Composite {
+        private MatrixBools.Params params; 
+        private Operators operator;
+        
+        public Composite(MatrixBools.Params p, Operators op) {
+            params = p;
+            operator = op;
+        } 
+           
+        public MatrixBools.Params getParams() { return params; }
+        public Operators getOperator() { return operator; }
     }
 
     public static void main(String[] args) {
@@ -64,25 +84,36 @@ public class FasterEvalDemo {
     }
     
     private static void showMessages(MatrixBools.Params params) {
-        Dispatcher theAnd = getDispatcher(params, THE_AND, Operators.AND);
-        Dispatcher theOr = getDispatcher(params, THE_OR, Operators.OR);
-        Dispatcher theXor = getDispatcher(params, THE_XOR, Operators.XOR);
+        Composite andArgs = new Composite(params, Operators.AND);
+        Composite orArgs = new Composite(params, Operators.OR);
+        Composite xorArgs = new Composite(params, Operators.XOR);
+        
+        Dispatcher theAnd = getDispatcher(andArgs);
+        Dispatcher theOr = getDispatcher(orArgs);
+        Dispatcher theXor = getDispatcher(xorArgs);
         
         System.out.println(RESULT + getTripleX(theAnd, theOr, theXor));
     }
     
-    private static Dispatcher 
-    getDispatcher(MatrixBools.Params params, String title, Operators op) {
+    private static Dispatcher getDispatcher(Composite params) {
         Dispatcher result = null;
         
-        switch (op) {
+        Operators operator = params.getOperator();
+        String title = operator.getTitle();
+        
+        MatrixBools.Params matrix = params.getParams();
+        boolean first = matrix.getFirst();
+        boolean second = matrix.getSecond();
+        boolean third = matrix.getThird();
+        
+        switch (operator) {
             case AND:
                 result = (messager) -> {
                     System.out.println(title);
                     return
-                        messager.message(GREETING, params.first) && 
-                        messager.message(ASK, params.second) && 
-                        messager.message(BYE, params.third);
+                        messager.message(GREETING, first) && 
+                        messager.message(ASK, second) && 
+                        messager.message(BYE, third);
                 };
                 break;
                 
@@ -90,9 +121,9 @@ public class FasterEvalDemo {
                 result = (messager) -> {
                     System.out.println(title);
                     return
-                        messager.message(GREETING, params.first) || 
-                        messager.message(ASK, params.second) || 
-                        messager.message(BYE, params.third);
+                        messager.message(GREETING, first) || 
+                        messager.message(ASK, second) || 
+                        messager.message(BYE, third);
                 };
                 break;
                 
@@ -100,9 +131,9 @@ public class FasterEvalDemo {
                 result = (messager) -> {
                     System.out.println(title);
                     return
-                        messager.message(GREETING, params.first) ^ 
-                        messager.message(ASK, params.second) ^ 
-                        messager.message(BYE, params.third);
+                        messager.message(GREETING, first) ^ 
+                        messager.message(ASK, second) ^ 
+                        messager.message(BYE, third);
                 };
                 break;           
         }
