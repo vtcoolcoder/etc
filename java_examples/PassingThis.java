070f7562892cfa627ce1f4104d7e80469b86f192
Vtszet <the@vtszet.com>
2023-11-07 16:20:07 +0300

    Добавил пример с практичным применением ключевого слова this -- для передачи текущего объекта внешнему методу.

---
+++ java_examples/PassingThis.java
@@ -0,0 +1,26 @@
+/* Ключевое слово this также может пригодиться 
+ * для передачи текущего объекта другому методу
+*/
+class Person {
+    public void eat(Apple apple) {
+        Apple peeled = apple.getPeeled();
+        System.out.println("Yummy");
+    }
+}
+
+class Peeler {
+    static Apple peel(Apple apple) {
+        // Обработка данных...
+        return apple;
+    }
+}
+
+class Apple {
+    Apple getPeeled() { return Peeler.peel(this); }
+}
+
+public class PassingThis {
+    public static void main(String[] args) {
+        new Person().eat(new Apple());
+    }
+}
