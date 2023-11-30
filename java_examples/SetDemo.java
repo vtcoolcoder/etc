7f5696b4ea3a0d7b0e9f2a3caaedbff838a66cca
Vtszet <the@vtszet.com>
2023-12-01 01:26:44 +0300

    Инкапсулировал операции над множествами в отдельные обобщённые статические методы.

--- java_examples/SetDemo.java
+++ java_examples/SetDemo.java
@@ -18,23 +18,27 @@ public class SetDemo {
         union.addAll(set2);
         System.out.print("Объединение множеств: ");
         System.out.println(union);
+        System.out.println("Проверка: " + getSetUnion(set1, set2));
         
         // пересечение множеств
         Set<Integer> intersection = new HashSet<>(set1);
         intersection.retainAll(set2);
         System.out.print("Пересечение множеств: ");
         System.out.println(intersection);
+        System.out.println("Проверка: " + getSetIntersection(set1, set2));
         
         // вычитание множеств
         Set<Integer> differenceSet2FromSet1 = new HashSet<>(set1);
         differenceSet2FromSet1.removeAll(set2);
         System.out.print("Разность множеств (первое - второе): ");
         System.out.println(differenceSet2FromSet1);
+        System.out.println("Проверка: " + getSetDifference(set1, set2));
         
         Set<Integer> differenceSet1FromSet2 = new HashSet<>(set2);
         differenceSet1FromSet2.removeAll(set1);
         System.out.print("Разность множеств (второе - первое): ");
         System.out.println(differenceSet1FromSet2);
+        System.out.println("Проверка: " + getSetDifference(set2, set1));
         
         // вычитание пересечения множеств из их объединения
         Set<Integer> unionTmp = new HashSet<>(set1);
@@ -46,5 +50,29 @@ public class SetDemo {
         
         System.out.print("Исключающее объединение множеств: ");   
         System.out.println(xorIntersection);
+        System.out.println("Проверка: " + getSetXorUnion(set1, set2));
+    }
+    
+    private static <T> Set<T> getSetXorUnion(Set<T> first, Set<T> second) {
+        return getSetDifference(getSetUnion(first, second), 
+                                getSetIntersection(first, second));
+    }
+    
+    private static <T> Set<T> getSetUnion(Set<T> first, Set<T> second) {
+        Set<T> tmp = new HashSet<>(first);
+        tmp.addAll(second);
+        return tmp;
+    }
+    
+    private static <T> Set<T> getSetIntersection(Set<T> first, Set<T> second) {
+        Set<T> tmp = new HashSet<>(first);
+        tmp.retainAll(second);
+        return tmp;
+    }
+    
+    private static <T> Set<T> getSetDifference(Set<T> first, Set<T> second) {
+        Set<T> tmp = new HashSet<>(first);
+        tmp.removeAll(second);
+        return tmp;
     }
 }
