6eb1b159ceee0a814a6a384ef4d981edada40e2f
Vtszet <the@vtszet.com>
2023-12-01 15:28:40 +0300

    Улучшил предыдущий пример, произвёл определённый рефакторинг.

--- java_examples/MultiThreadsDemo.java
+++ java_examples/MultiThreadsDemo.java
@@ -1,77 +1,76 @@
 public class MultiThreadsDemo {
+    @FunctionalInterface
+    private interface MyFuncOnThread {
+        void func(Thread thread) throws InterruptedException;
+    }
+
+
     private static class MyThread extends Thread {
         @Override
-        public void run() {
-            for (int i = 0; i <= 23; i++) { 
-                System.out.println("classExtendsThread: " + i); 
-            }
-        }
+        public void run() { printMessages("classExtends"); }
     }
     
      
     private static class MyAnotherThread implements Runnable {
         @Override
-        public void run() {
-            for (int i = 0; i < 23; i++) {
-                System.out.println("classImplementsThread: " + i);
-            }
-        }
+        public void run() { printMessages("classImplements"); }
     }
 
 
     public static void main(String[] args) {
         Runnable runnable = new Runnable() {
             @Override
-            public void run() {
-                for (int i = 0; i < 23; i++) {
-                    System.out.println("anotherAnonymousClassThread: " + i);
-                }
-            }
+            public void run() { printMessages("anotherAnonymousClass"); }
         };        
     
         Thread anonymousClassThread = new Thread() {
             @Override
-            public void run() {
-                for (int i = 0; i < 23; i++) { 
-                    System.out.println("anonymousClassThread: " + i); 
-                }
-            }
+            public void run() { printMessages("anonymousClass"); }
         };  
         
-        Thread lambdaThread = new Thread(() -> {
-            for (int i = 1; i <= 23; i++) { 
-                System.out.println("lambdaThread: " + i); 
-            }
-        });
-        
+        Thread lambdaThread = new Thread(() -> printMessages("lambda"));
         Thread anotherAnonymousClassThread = new Thread(runnable); 
         Thread refByMethodThread = new Thread(MultiThreadsDemo::showMessages); 
         Thread classExtendsThread = new MyThread();
         Thread classImplementsThread = new Thread(new MyAnotherThread());
-            
-        startThreads(lambdaThread, 
-                     refByMethodThread, 
-                     anonymousClassThread,
-                     classImplementsThread,
-                     classExtendsThread,
-                     anotherAnonymousClassThread);
         
-        for (int i = 0; i < 23; i++) { 
-            System.out.println("mainThread: " + i); 
-        }
+        Thread[] threadsList = {
+            lambdaThread, 
+            refByMethodThread, 
+            anonymousClassThread,
+            classImplementsThread,
+            classExtendsThread,
+            anotherAnonymousClassThread
+        };
+            
+        startThreads(threadsList, 
+                     thread -> thread.start(),
+                     thread -> thread.join());  
+        
+        printMessages("main");
     }  
     
     
-    private static void showMessages() {
-        for (int i = -23; i <= 0; i++) { 
-            System.out.println("methodThread: " + i); 
+    private static void showMessages() { printMessages("method"); }
+    
+    
+    private static synchronized void printMessages(String name) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(name + "Thread: " + i);
         }
-    } 
+    }
     
     
-    private static void startThreads(Thread... threads) {
-        for (Thread thread : threads) { 
-            thread.start(); 
+    private static void startThreads(Thread[] threads, MyFuncOnThread... myFuncs) {       
+        for (MyFuncOnThread myFunc : myFuncs) {
+            try {
+                for (Thread thread : threads) { 
+                    myFunc.func(thread);
+                }
+            }
+            catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 }
