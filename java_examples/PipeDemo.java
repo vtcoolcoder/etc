9449b406ebcb7b97a6117d46c5ff3a1a297fae4f
Vtszet <the@vtszet.com>
2023-12-08 01:59:32 +0300

    Усложнил пример: теперь можно обрабатывать текст построчно на лету, применяя к каждой строке массив из лямбда-выражений. Пока остановился на простом реверсе...

--- java_examples/PipeDemo.java
+++ java_examples/PipeDemo.java
@@ -1,11 +1,42 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class PipeDemo {
+    @FunctionalInterface
+    public interface Textable {
+        StringBuffer process(StringBuffer input);
+    }
+
+
     public static void main(String[] args) {
+        runEcho(PipeDemo::wrapReverse);     
+    }
+    
+    
+    private static StringBuffer wrapReverse(StringBuffer item) { 
+        return item != null ? item.reverse() : new StringBuffer();
+    }
+    
+    
+    private static void runEcho(Textable... funcs) {
         try (Scanner scanner = new Scanner(System.in)) {
             while (scanner.hasNextLine()) {
-                System.out.println(scanner.nextLine());
+                processText(scanner.nextLine(), funcs)
+                    .stream().forEach(System.out::println);
             }
         }
+    }
+    
+    
+    private static List<String> processText(String input, Textable... funcs) {
+        List<String> result = new ArrayList<>();
+        StringBuffer buffer = new StringBuffer(input != null ? input : "");
+        
+        for (Textable func : funcs) {
+            result.add(func.process(buffer).toString());
+        }
+        
+        return result;
     }
 }
