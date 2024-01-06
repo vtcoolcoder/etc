30de4a3ec15b42dffd98923c78475f7aeaf9e44d
Vtszet <the@vtszet.com>
2024-01-06 16:19:23 +0300

    Доработал record Student: в частности, сделал валидацию входящих параметров в конструкторе. Также теперь нельзя создавать несколько экземпляров с одним и тем же id.

--- java_examples/ShowStudents.java
+++ java_examples/ShowStudents.java
@@ -1,56 +1,4 @@
 public class ShowStudents {
-    public record Student(
-            int id,
-            String name,
-            String surname,
-            int age,
-            int height,
-            int weight) 
-    {
-        private static int counter = 0;
-        
-        public Student(
-                String name,
-                String surname,
-                int age,
-                int height,
-                int weight)
-        {
-            this(++counter, name, surname, age, height, weight);
-        }
-    
-    
-        @Override
-        public String toString() {
-            final String FORMAT = """
-            id: %d
-            Имя: %s
-            Фамилия: %s
-            Возраст: %d
-            Рост: %d
-            Вес: %d
-            """;
-            
-            return String.format(FORMAT, id, name, surname, age, height, weight);
-        }
-        
-        
-        public static int getAmount() { return counter; }
-        
-        
-        public static void showInfo() {
-            System.out.println("Всего студентов: " + counter);
-        }
-        
-        
-        public static void show(Student... students) {
-            for (Student student : students) {
-                System.out.println(student);
-            }
-        }
-    }
-
-
     private static final Student[] STUDENTS = {
             new Student("Аня", "Романова", 21, 170, 55),
             new Student("Диана", "Липатова", 19, 175, 60),
@@ -62,10 +10,11 @@ public class ShowStudents {
             new Student("Елизавета", "Максимова", 25, 175, 55),
             new Student("Юлия", "Жукова", 26, 180, 60),
     };
-
-
+    
+    
     public static void main(String[] args) {
         Student.showInfo();
+        System.out.println();
         Student.show(STUDENTS);
     }
 }
---
+++ java_examples/Student.java
@@ -0,0 +1,151 @@
+import java.util.Set;
+import java.util.HashSet;
+
+
+public record Student(
+        int id,
+        String name,
+        String surname,
+        int age,
+        int height,
+        int weight) 
+{
+    private static final Set<Integer> existingUniqueId = new HashSet<>();
+    
+    private static final int IDMIN = 1;
+    private static final int AGEMIN = 1;
+    private static final int AGEMAX = 120;
+    private static final int HEIGHTMIN = 75;
+    private static final int HEIGHTMAX = 250;
+    private static final int WEIGHTMIN = 20;
+    private static final int WEIGHTMAX = 300;
+    
+    private static final String AMOUNT = "Всего студентов: ";
+    private static final String MUSTBE = "должен быть"; 
+    private static final String WRONGIDRANGE = String.format("id %s не меньше %d!", MUSTBE, IDMIN);
+    private static final String WRONGIDUNIQUE = "id: %d уже существует!";
+    private static final String PREFIX = "Некорректн";
+    private static final String WRONGNAME = PREFIX + "ое имя!";
+    private static final String WRONGSURNAME = PREFIX + "ая фамилия!";
+    private static final String AHWFMT = "%s " + MUSTBE + " в диапазоне от %d до %d включительно!";
+    private static final String WRONGAGE = String.format(AHWFMT, "Возраст", AGEMIN, AGEMAX);
+    private static final String WRONGHEIGHT = String.format(AHWFMT, "Рост", HEIGHTMIN, HEIGHTMAX);
+    private static final String WRONGWEIGHT = String.format(AHWFMT, "Вес", WEIGHTMIN, WEIGHTMAX);
+    
+    private static final String REGEXP = """
+    [AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtVvUuWwXxYyZz\
+    АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя]+""";
+    
+    private static final String SHOWFORMAT = """
+    id: %d
+    Имя: %s
+    Фамилия: %s
+    Возраст: %d
+    Рост: %d
+    Вес: %d
+    """;
+
+
+    private static int counter = 0;
+    
+    
+    public Student(
+            final String name,
+            final String surname,
+            final int age,
+            final int height,
+            final int weight)
+    {
+        this(++counter, name, surname, age, height, weight);
+    }
+    
+    
+    public Student {
+        checkId(id);
+        checkName(name = name.trim());
+        checkSurname(surname = surname.trim());
+        checkAge(age);
+        checkHeight(height);
+        checkWeight(weight);
+           
+        existingUniqueId.add(id);
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format(SHOWFORMAT, id, name, surname, age, height, weight);
+    }
+    
+    
+    public static int getAmount() { return counter; }
+    
+    
+    public static void showInfo() {
+        System.out.println(AMOUNT + counter);
+    }
+    
+    
+    public static void show(final Student... students) {
+        for (Student student : students) {
+            System.out.println(student);
+        }
+    }
+    
+    
+    private static void checkId(final int id) {
+        if (id < IDMIN) {
+            throw new IllegalArgumentException(WRONGIDRANGE);
+        } else if (existingUniqueId.contains(id)) {
+            throw new IllegalArgumentException(String.format(WRONGIDUNIQUE, id));
+        }
+    }
+    
+    
+    private static void checkName(final String name) { 
+        checkNameOrSurname(name, WRONGNAME); 
+    }
+    
+    
+    private static void checkSurname(final String surname) { 
+        checkNameOrSurname(surname, WRONGSURNAME); 
+    }
+    
+    
+    private static void checkAge(final int age) { 
+        checkAgOrHtOrWt(age, AGEMIN, AGEMAX, WRONGAGE); 
+    }
+    
+    
+    private static void checkHeight(final int height) { 
+        checkAgOrHtOrWt(height, HEIGHTMIN, HEIGHTMAX, WRONGHEIGHT); 
+    }
+    
+    
+    private static void checkWeight(final int weight) { 
+        checkAgOrHtOrWt(weight, WEIGHTMIN, WEIGHTMAX, WRONGWEIGHT); 
+    }
+    
+    
+    private static void checkNameOrSurname(final String nameOrSurname, 
+                                           final String errorMsg) 
+    {
+        if (! nameOrSurname.matches(REGEXP)) 
+        {
+            throw new IllegalArgumentException(errorMsg);
+        }
+    }
+    
+    
+    private static void checkAgOrHtOrWt(final int ageOrHeightOrWeight,
+                                        final int min, 
+                                        final int max,
+                                        final String errorMsg) 
+    {
+        if (ageOrHeightOrWeight < min || 
+            ageOrHeightOrWeight > max) 
+        {
+            throw new IllegalArgumentException(errorMsg);
+        }
+    }
+}
