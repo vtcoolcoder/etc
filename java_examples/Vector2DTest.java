41d75cc86acaaf97e7e837ef0fcdd1dd94c3fea8
Vtszet <the@vtszet.com>
2024-04-10 01:47:34 +0300

    Исправил некоторые баги, допущенные по невнимательности; произвёл небольшой рефакторинг.

--- java_examples/Vector2DTest.java
+++ java_examples/Vector2DTest.java
@@ -5,38 +5,88 @@ import java.util.Arrays;
 
 import org.junit.Test;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 
 
 public class Vector2DTest {
+    private static final double EPS = 1E-9;
+    
+    private static final int MIN_ORDINAL_NUMBER = 1;
+    private static final int DEFAULT_VALUE = 0;
+    
     private static final String TEST_METHODS_NAME_PREFIX = "test";
     private static final String TEMPLATE = "[ТЕСТ]: %%s\n[СТАТУС]: %%s\n%s\n";
-    private static final String ERRFORMATTING = "[ERROR]: %s\n";
+    private static final String ERRFORMATTING = "[ПРИЧИНА]: %s\n";
     private static final String OK_FORMAT = TEMPLATE.formatted("");
     private static final String FAIL_FORMAT = TEMPLATE.formatted(ERRFORMATTING);
     private static final String STATUS_OK = "OK";
     private static final String STATUS_FAIL = "FAIL";
+    private static final String ILLEGAL_ARG_MSG = 
+            "Порядковый номер аргумента командной строки не должен быть меньше %d!"
+            .formatted(MIN_ORDINAL_NUMBER);
+    
+    
+    private static Vector2D vector; 
       
     private static int expectedIntoNewVectorShouldHaveZeroLength;
     private static int expectedIntoNewVectorShouldHaveZeroX;
     private static int expectedIntoNewVectorShouldHaveZeroY;
     
+    
+    static {
+        createNewVector();
+    }
+    
 
     public static void main(String[] args) {
+        initExpectings(args);
+        runManualTesting();
+    }
+    
+    
+    @BeforeClass
+    public static void createNewVector() { vector = new Vector2D(); }
+    
+    
+    @Test
+    public void testNewVectorShouldHaveZeroLength() {
+        Assert.assertEquals(expectedIntoNewVectorShouldHaveZeroLength, 
+                vector.length(), EPS);
+    }
+    
+    
+    @Test
+    public void testNewVectorShouldHaveZeroX() {
+        Assert.assertEquals(expectedIntoNewVectorShouldHaveZeroX, 
+                vector.getX(), EPS);
+    }
+    
+    
+    @Test
+    public void testNewVectorShouldHaveZeroY() {
+        Assert.assertEquals(expectedIntoNewVectorShouldHaveZeroY, 
+                vector.getY(), EPS);
+    }
+    
+    
+    private static void initExpectings(final String[] args) {
         try {
-            expectedIntoNewVectorShouldHaveZeroLength = 
-                    args.length > 0 ? Integer.parseInt(args[0]) : 0;
-                    
-            expectedIntoNewVectorShouldHaveZeroX = 
-                    args.length > 1 ? Integer.parseInt(args[1]) : 0;
-                    
-            expectedIntoNewVectorShouldHaveZeroY = 
-                    args.length > 2 ? Integer.parseInt(args[2]) : 0;
+            expectedIntoNewVectorShouldHaveZeroLength = getCLArgByOrdinalNumber(args, 1);
+            expectedIntoNewVectorShouldHaveZeroX = getCLArgByOrdinalNumber(args, 2);
+            expectedIntoNewVectorShouldHaveZeroY = getCLArgByOrdinalNumber(args, 3);               
         } catch (NumberFormatException e) {}
+    }
     
     
+    private static int getCLArgByOrdinalNumber(final String[] args, final int ordinalNumber) {
+        if (ordinalNumber < MIN_ORDINAL_NUMBER) { throw new IllegalArgumentException(ILLEGAL_ARG_MSG); }
+        final int INDEX = ordinalNumber - 1;
+        return args.length > INDEX ? Integer.parseInt(args[INDEX]) : DEFAULT_VALUE;
+    }
+    
+    
+    private static void runManualTesting() {
         Vector2DTest test = new Vector2DTest();
-        
-        
         Method[] methods = test.getClass().getMethods();
         
         Arrays.stream(methods)
@@ -46,7 +96,7 @@ public class Vector2DTest {
                   
                   try {
                       method.invoke(test);
-                      System.err.printf(OK_FORMAT, methodName, STATUS_OK);
+                      System.out.printf(OK_FORMAT, methodName, STATUS_OK);
                   } catch (InvocationTargetException outer) {
                   
                       try {
@@ -55,38 +105,11 @@ public class Vector2DTest {
                           System.err.printf(FAIL_FORMAT, methodName, 
                                   STATUS_FAIL, inner.getMessage());
                       } catch (Throwable inner) {
-                          new RuntimeException(inner);
+                          throw new RuntimeException(inner);
                       }
                   } catch (Exception outer) {
-                      new RuntimeException(outer);
+                      throw new RuntimeException(outer);
                   }
               });
-    }
-    
-    
-    @Test
-    public void testNewVectorShouldHaveZeroLength() {
-        Vector2D vector = new Vector2D(); // action
-        // assertion
-        Assert.assertEquals(expectedIntoNewVectorShouldHaveZeroLength, 
-                vector.length(), 1E-9);
-    }
-    
-    
-    @Test
-    public void testNewVectorShouldHaveZeroX() {
-        Vector2D vector = new Vector2D();
-        
-        Assert.assertEquals(expectedIntoNewVectorShouldHaveZeroX, 
-                vector.getX(), 1E-9);
-    }
-    
-    
-    @Test
-    public void testNewVectorShouldHaveZeroY() {
-        Vector2D vector = new Vector2D();
-        
-        Assert.assertEquals(expectedIntoNewVectorShouldHaveZeroY, 
-                vector.getY(), 1E-9);
     }
 }
