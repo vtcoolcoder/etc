e22a4154ae9c9b4c32baa114ce996101e9bd6e9e
Vtszet <the@vtszet.ru>
2024-05-06 18:47:01 +0300

    API для класса ServiceServlet.

---
+++ WEB-INF/classes/using_spring/WebNotesAPI.java
@@ -0,0 +1,32 @@
+package using_spring;
+
+
+import java.util.Map;
+import java.util.Set;
+
+
+public interface WebNotesAPI {
+    void createNote(final Note... notes); 
+     
+    Set<Note> getAllNotes();  
+           
+    Map<String, Set<Note>> getNotes(final String subject);   
+        
+    Map<String, Set<Note>> getNotes(final Set<String> subjects);  
+    
+    String getNoteContent(final int id); 
+    
+    Map<String, Integer> getNotesBySubjectAmount();
+    
+    Set<String> getAllSubjects();   
+    
+    int getAllSubjectsAmount();    
+    
+    int getAllNotesAmount();   
+    
+    Note getRandomNote();
+    
+    void updateNote(final String content, final int id);
+    
+    void deleteNote(final int id);         
+}
