cd30e6f074cb4f6e42c34a061e76e379aa711112
Vtszet <the@vtszet.ru>
2024-05-08 04:03:26 +0300

    Test

--- WEB-INF/classes/using_spring/SpringConfig.java
+++ WEB-INF/classes/using_spring/SpringConfig.java
@@ -1127,7 +1127,7 @@ public class SpringConfig {
     }
     
     
-    private static <R> R useTemplate(SQLFunctionalInterface action) {
+    private static <T implements SQLFunctionalInterface, R> R useTemplate(T action) {
         try {
             if (action instanceof SQLRunnable runnable) {
                 runnable.run();
